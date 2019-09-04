505b6576990718d92efa994378627456bfffd1ee
Charlie Chung <charlieschung@gmail.com>
2019-09-04 19:01:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.detect do |cheese|cheese 
+  array.detect do |cheese|
+    
     
   end
   cheese_types = ["cheddar", "gouda", "camembert"]
