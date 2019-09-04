ff832feffbefa89aceacffcb3d7ce3e2e435acad
Charlie Chung <charlieschung@gmail.com>
2019-09-04 19:12:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,7 @@ end
 
 def find_the_cheese(array)
  cheese_types = ["cheddar", "gouda", "camembert"]
- if array.include?(cheese_types)
+ array.detect do |cheese|
+  cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+ end
 end
